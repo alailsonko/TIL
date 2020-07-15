0a13d244a9af72d61539ff0ad39579e8d565401e
alailsonko <aalailson3@gmail.com>
2020-07-15 06:39:29 -0300

    chore: configuring the first steps for start development

---
+++ first-project/backend/core/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'hello gostack '})
+})
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+  
+})
