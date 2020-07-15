59e420128ac58ab9cf725ee07ed9172027b57f49
alailsonko <aalailson3@gmail.com>
2020-07-15 14:16:05 -0300

    feat: creating new routes for create appointments

--- first-project/backend/core/src/routes/index.ts
+++ first-project/backend/core/src/routes/index.ts
@@ -1,11 +1,32 @@
 import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
-const routes = Router();
+const appointmentsRouter = Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'hello gostack',
-  }),
-);
+appointmentsRouter.get('/appointments', (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = appointmentsRepository.find();
 
-export default routes;
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/appointments', async (req, res) => {
+  try {
+    const { provider, date } = req.body;
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+    const appointment = await createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
