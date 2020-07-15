729cafd856ffc19f7b526e716b4f3511d059ec4f
alailsonko <aalailson3@gmail.com>
2020-07-15 07:07:42 -0300

    chore: implementing eslint on the core of project

--- first-project/backend/core/src/server.ts
+++ first-project/backend/core/src/server.ts
@@ -1,12 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'hello gostack '})
-})
+app.get('/', (req, res) => res.json({
+  message: 'hello gostack',
+}));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
-
-})
+});
