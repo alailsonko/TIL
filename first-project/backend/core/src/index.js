dbbc31bd9115898d630dfc5c7cae89a2f939eb17
alailsonko <aalailson3@gmail.com>
2020-07-23 11:22:15 -0300

    studies about get, post, put, delete

--- first-project/backend/core/src/index.js
+++ first-project/backend/core/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
@@ -15,41 +16,69 @@ const port = 3333
 
  /**
   * types of parameters
-  * 
+  *
   * Query params: filtros e paginação
   * Route params: identify the resources which to delete
   * Request Body: content of body for edit or create one resource
   */
 
+const projects = [];
 
 app.get('/projects', (req, res) => {
-    return res.json([
-        'Projeto 1',
-        'Projeto 2'
-    ])
+
+  const { title } = req.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+    return res.json(results)
 })
 
 app.post('/projects', (req, res) => {
-    return res.json([
-        'Projeto 1',
-        'Projeto 2',
-        'projeto 3'
-    ])
+    const { title, owner } = req.body;
+    const project = { id: uuid(), title, owner }
+
+    projects.push(project)
+
+    return res.json(project)
 })
 
 app.put('/projects/:id', (req, res) => {
-    return res.json([
-        'Projeto 4',
-        'Projeto 2',
-        'projeto 3'
-    ])
+
+    const { id } = req.params
+
+    const projectIndex = projects.find(project => project.id === id)
+
+    if ( projectIndex < 0){
+      return res.status(400).json({ error: 'Project not found'})
+    }
+
+    const project = {
+      id,
+      title,
+      owner
+    }
+
+    projects[projectIndex] = project;
+
+    return res.json(project)
 })
 
 app.delete('/projects/:id', (req, res) => {
-    return res.json([
-        'Projeto 1',
-        'Projeto 2',
-    ])
+
+  const { id } = req.params
+
+  const projectIndex = projects.find(project => project.id === id)
+
+  if ( projectIndex < 0){
+    return res.status(400).json({ error: 'Project not found'})
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return res.status(204).send()
+
 })
 
 app.listen(port, () => {
