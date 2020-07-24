eb6035654521d8bb139bc37d23fa2b448de21653
alailsonko <aalailson3@gmail.com>
2020-07-24 01:16:34 -0300

    creating the relation between appointments and users

---
+++ first-project/backend/core/src/routes/users.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.json(400).json({ error: err.message });
+  }
+});
