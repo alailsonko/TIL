8673e1edc172d707844583f385d5aa94cff86e28
alailsonko <aalailson3@gmail.com>
2020-07-24 13:22:28 -0300

    implemetning user services

--- first-project/backend/core/src/routes/index.ts
+++ first-project/backend/core/src/routes/index.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
