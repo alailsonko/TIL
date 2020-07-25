3492dbfa7c0a9659c22493e47340a039c389b7be
alailsonko <aalailson3@gmail.com>
2020-07-24 21:27:27 -0300

    refactoring for DDD

--- first-project/backend/core/src/modules/users/services/AuthenticationUserService.ts
+++ first-project/backend/core/src/modules/users/services/AuthenticationUserService.ts
@@ -1,10 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import authConfig from '../config/auth';
-import User from '../models/User';
+import authConfig from '../../../config/auth';
+import User from '../entities/User';
 
-import AppError from '../errors/AppError';
+import AppError from '../../../shared/errors/AppError';
 
 interface Request {
   email: string;
--- first-project/backend/core/src/server.ts
+++ first-project/backend/core/src/server.ts
@@ -2,11 +2,11 @@ import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import routes from './routes';
+import routes from './shared/routes';
 import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import AppError from './shared/errors/AppError';
 
-import './database';
+import './shared/database';
 
 const app = express();
 
