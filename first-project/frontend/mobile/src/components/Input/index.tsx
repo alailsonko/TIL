1c4c4f561b4d006b58463492b7438f9d9fdcb306
alailsonko <aalailson3@gmail.com>
2020-07-26 12:29:00 -0300

    styling button and input

--- first-project/frontend/mobile/src/components/Input/index.tsx
+++ first-project/frontend/mobile/src/components/Input/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextInputProps } from 'react-native'
 
-import { Container, TextInput } from './styles';
+import { Container, TextInput, Icon } from './styles';
 
 interface InputProps extends TextInputProps {
     name: string;
@@ -11,6 +11,7 @@ interface InputProps extends TextInputProps {
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   return (
     <Container>
+      <Icon name={icon} size={20} color="#666360" />
       <TextInput
       placeholderTextColor="#666360"
       {...rest}
--- first-project/frontend/mobile/src/components/Input/styles.ts
+++ first-project/frontend/mobile/src/components/Input/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather'
+
 
 export const Container = styled.View`
     width: 100%;
@@ -7,6 +9,9 @@ export const Container = styled.View`
     background: #232129;
     border-radius: 10px;
     margin-bottom: 8px;
+
+    flex-direction: row;
+    align-items: center;
 `;
 
 export const TextInput = styled.TextInput`
@@ -14,4 +19,7 @@ export const TextInput = styled.TextInput`
    color: #fff;
    font-size: 16px;
    font-family: 'RobotoSlab-Regular';
+`
+export const Icon = styled(FeatherIcon)`
+   margin-right: 16px;
 `
