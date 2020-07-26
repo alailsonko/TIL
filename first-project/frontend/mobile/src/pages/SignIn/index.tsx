9e5d067da5ac8bf62511cbc28f415a0639cc8c46
alailsonko <aalailson3@gmail.com>
2020-07-25 22:53:22 -0300

    button and input compnents created

--- first-project/frontend/mobile/src/pages/SignIn/index.tsx
+++ first-project/frontend/mobile/src/pages/SignIn/index.tsx
@@ -3,6 +3,9 @@ import { Image } from 'react-native'
 
 import { Container, Title } from './styles';
 
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
 import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
@@ -10,6 +13,12 @@ const SignIn: React.FC = () => {
   <Container>
     <Image source={logoImg} />
     <Title>Faça seu logon</Title>
+
+    <Input />
+    <Input />
+    
+    <Button>Entrar</Button>
+
   </Container>);
 }
 
