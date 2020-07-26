bc1af35640bd54a052a662ab928b581ce2eff038
alailsonko <aalailson3@gmail.com>
2020-07-26 13:16:40 -0300

    styling the forgot password buttona dn create account button

--- first-project/frontend/mobile/src/pages/SignIn/index.tsx
+++ first-project/frontend/mobile/src/pages/SignIn/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
-import { Image } from 'react-native'
+import {
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  View
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles';
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -10,17 +24,47 @@ import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
   return (
+    <>
+    <KeyboardAvoidingView
+    style={{ flex: 1}}
+    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    enabled
+    >
+      <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
   <Container>
     <Image source={logoImg} />
+    <View>
     <Title>Faça seu logon</Title>
+    </View>
 
     <Input name="email" icon="mail" placeholder="E-mail" />
     <Input name="password" icon="lock" placeholder="password" />
 
-    <Button onPress={() => console.log('hello world')
-    }>Entrar</Button>
+    <Button
+    onPress={() => console.log('hello world')}
+    >Entrar</Button>
 
-  </Container>);
+    <ForgotPassword
+    onPress={() => console.log('forgot password')}>
+
+      <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+
+    </ForgotPassword>
+
+    </Container>
+    </ScrollView>
+    </KeyboardAvoidingView>
+     <CreateAccountButton
+     onPress={() => console.log('create account')}>
+
+        <Icon name="log-in" size={20} color="#ff9000" />
+
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+
+     </CreateAccountButton>
+
+  </>
+  )
 }
 
 
