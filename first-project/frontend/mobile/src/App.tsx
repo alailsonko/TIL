32c7b942f304644ade8966249430f1a0b4672ab8
alailsonko <aalailson3@gmail.com>
2020-07-25 22:15:41 -0300

    fetching async fonts on the system

--- first-project/frontend/mobile/src/App.tsx
+++ first-project/frontend/mobile/src/App.tsx
@@ -1,21 +1,45 @@
 import 'react-native-gesture-handler'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
 import Routes from './routes'
 
-const App: React.FC = () => (
-    <NavigationContainer> 
-    <StatusBar barStyle="light-content" />
-    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-       <Routes />
-    </View>
-    
-    </NavigationContainer>
-
-)
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 
+
+function App(): React.ReactNode {
+    const [dataLoaded, setDataLoaded] = useState(false)
+
+
+
+    const fetchFonts = () => {
+        return Font.loadAsync({
+        'RobotoSlab-Medium': require('../assets/fonts/RobotoSlab-Medium.ttf'),
+        'RobotoSlab-Regular': require('../assets/fonts/RobotoSlab-Regular.ttf')
+        });
+        };
+
+  if(!dataLoaded) {
+    return(
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={()=>setDataLoaded(true)}
+      />
+    )
+  }
+
+    return (
+        <NavigationContainer>
+        <StatusBar barStyle="light-content" />
+        <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+           <Routes />
+        </View>
+
+        </NavigationContainer>
+    )
+}
 export default App;
--- first-project/frontend/mobile/src/pages/SignIn/styles.ts
+++ first-project/frontend/mobile/src/pages/SignIn/styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components/native';
 
+
 export const Container = styled.View`
     
 `;
+
+export const Title = styled.Text`
+   font-size: 20px;
+   color: #f4ede8;
+   font-family: 'RobotoSlab-Medium';
+`
