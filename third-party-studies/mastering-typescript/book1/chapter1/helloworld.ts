91ebd99c49e73841f97348e7c0cbb1fe1c52fa0d
alailsonko <aalailson3@gmail.com>
2020-07-27 11:13:06 -0300

    type annotation

--- third-party-studies/mastering-typescript/book1/chapter1/helloworld.ts
+++ third-party-studies/mastering-typescript/book1/chapter1/helloworld.ts
@@ -1,1 +1,42 @@
 console.log('hello world');
+
+var radius = 4;
+var area = Math.PI * radius * radius;
+
+area
+
+// radius = 'A string' # static type checking
+
+//Tpe annotation
+
+// primitive type annotation
+var nameN: string = "Steve";
+nameN
+var heigthtCM: number = 182.22;
+heigthtCM
+var isActive: boolean = true
+isActive
+
+// array type annotation
+var names: string[] = ['james', 'nick', 'rebeca', 'lily']
+names
+
+// function annotation with parameter type annotation and return type annotation
+var sayHello: (name: string) => string;
+
+// implementio of sayHello function
+sayHello = function (name: string) {
+    return 'Hello' + name;
+}
+const hello1 = sayHello(' one string')
+hello1
+
+// object type annotation
+var person: { name: string; heightCM: number}
+
+// IMplementtation of a person object
+person = {
+    name: 'one name',
+    heightCM: 437
+}
+person
