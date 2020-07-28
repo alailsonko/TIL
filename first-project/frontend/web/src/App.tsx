b0be0149b5d429b14059e05705e9e1f93af5219c
alailsonko <aalailson3@gmail.com>
2020-07-28 00:17:44 -0300

    styling the login page

--- first-project/frontend/web/src/App.tsx
+++ first-project/frontend/web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/Global';
+import SignIn from './pages/SignIn';
 
-const App: React.FC = () => <h1>hello world</h1>;
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
