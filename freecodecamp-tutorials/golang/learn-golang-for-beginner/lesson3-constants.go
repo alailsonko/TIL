56cf9541dcf7d5f76ffeee97dbc53d037000165e
alailsonko <aalailson3@gmail.com>
2020-09-25 01:56:36 -0300

    lessont3

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson3-constants.go
@@ -0,0 +1,41 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // const (
+// // 	a = iota
+// // 	b = iota
+// // 	c = iota
+// // )
+
+// // const (
+// // 	a2 = iota
+// // )
+
+// const (
+// 	_  = iota
+// 	KB = 1 << (10 * iota)
+// 	MB
+// 	GB
+// 	TB
+// 	PB
+// 	EB
+// 	ZB
+// 	YB
+// )
+
+// func main() {
+
+// 	fileSize := 4000000000
+// 	// var specialistType int
+// 	fmt.Printf("%v.2fGB", fileSize/GB)
+
+// 	// const a = 42
+// 	// var b int16 = 27
+// 	// fmt.Printf("%v, %T\n", a, a)
+// 	// fmt.Printf("%v, %T\n", b, b)
+// 	// fmt.Printf("%v, %T\n", c, c)
+// 	// fmt.Printf("%v, %T\n", a2, a2)
+// }
