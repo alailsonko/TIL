893fb4e8472f9638ea53f6026fe83250a19d388b
alailsonko <aalailson3@gmail.com>
2020-09-24 23:48:14 -0300

    variables in go

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson1-variables.go
@@ -0,0 +1,47 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
+
+// // var (
+// // actorName    string = "Elisabeth Sladen"
+// // companion    string = "Sarah Jane Smith"
+// // doctorNumber int    = 3
+// // season       int    = 11
+// // )
+
+// // var i float32 = 42
+
+// // var i int = 27
+
+// // var I int = 42
+
+// func main() {
+// 	// fmt.Printf("%v, %T", i, i)
+
+// 	// var seasonNumber int = 11
+
+// 	var i int = 42
+// 	fmt.Printf("%v, %T\n", i, i)
+// 	fmt.Printf("\n")
+// 	var j string
+// 	j = strconv.Itoa(i)
+// 	fmt.Printf("%v, %T\n", j, j)
+
+// 	// var theURL string = "https://google.com"
+// 	// var theHTTPRequest string =
+
+// 	// j := 13
+
+// 	// i := 77
+// 	// var i int
+// 	// i = 42
+// 	// var j int = 27
+// 	// k := 99.
+// 	// i := 42
+// 	// i = 42
+// 	// i = 27
+// 	// fmt.Println(i)
+// }
