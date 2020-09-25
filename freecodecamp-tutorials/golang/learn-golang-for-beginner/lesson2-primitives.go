cbb6c21a0d93be73054273361bf1b1801615ce32
alailsonko <aalailson3@gmail.com>
2020-09-24 23:48:25 -0300

    primitives types in go

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson2-primitives.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s := "this is a string"
+	b := []byte(s)
+	fmt.Printf("%v, %T\n", b, b)
+
+	// var n complex128 = 1 + 2i
+	// var n complex128 = complex(5, 12)
+	// fmt.Printf("%v, %T\n", n, n)
+
+	// fmt.Printf("%v, %T\n", real(n), real(n))
+	// fmt.Printf("%v, %T\n", imag(n), imag(n))
+
+	// a := 10.2
+	// b := 3.7
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+
+	// var n float64 = 3.14
+	// n = 13.7e72
+	// n = 2.1e14
+	// fmt.Printf("%v, %T\n", n, n)
+
+	// a := 8
+
+	// fmt.Println(a << 3)
+	// fmt.Println(a >> 3)
+
+	// a := 10
+	// b := 3
+
+	// fmt.Println(a & b)
+	// fmt.Println(a | b)
+	// fmt.Println(a ^ b)
+	// fmt.Println(a &^ b)
+
+	// var a int = 10
+	// var b int8 = 3
+	// fmt.Println(a + b)
+
+	// a := 18
+	// b := 3
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+	// fmt.Println(a % b)
+
+	// n := 42
+	// var n uint64 = 42
+	// var n bool
+	// n := 1 == 2
+	// m := 1 == 1
+	// fmt.Printf("%v, %T\n", n, n)
+	// fmt.Printf("\n")
+	// fmt.Printf("%v, %T\n", m, m)
+}
