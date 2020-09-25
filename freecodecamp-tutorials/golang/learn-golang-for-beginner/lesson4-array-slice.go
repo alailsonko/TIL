1359215b01507d0de29c211e54582354097dc5ab
alailsonko <aalailson3@gmail.com>
2020-09-25 01:56:55 -0300

    lesson4

--- freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson4-array-slice.go
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson4-array-slice.go
@@ -1,48 +1,48 @@
-package main
+// package main
 
-func main() {
+// func main() {
 
-	// a := []int{}
-	// fmt.Println(a)
-	// fmt.Println("Length: %v\n", len(a))
-	// fmt.Println("Capacity: %v\n", cap(a))
-	// a = append(a, 1)
-	// fmt.Println(a)
-	// fmt.Println("Length: %v\n", len(a))
-	// fmt.Println("Capacity: %v\n", cap(a))
-	// a = append(a, 2, 3, 4, 5)
-	// fmt.Println(a)
-	// fmt.Println("Length: %v\n", len(a))
-	// fmt.Println("Capacity: %v\n", cap(a))
-	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	// b := a[:]
-	// c := a[3:]
-	// d := a[:6]
-	// e := a[3:6]
-	// a[5] = 42
-	// fmt.Println(a)
-	// fmt.Println(b)
-	// fmt.Println(c)
-	// fmt.Println(d)
-	// fmt.Println(e)
+// 	// a := []int{}
+// 	// fmt.Println(a)
+// 	// fmt.Println("Length: %v\n", len(a))
+// 	// fmt.Println("Capacity: %v\n", cap(a))
+// 	// a = append(a, 1)
+// 	// fmt.Println(a)
+// 	// fmt.Println("Length: %v\n", len(a))
+// 	// fmt.Println("Capacity: %v\n", cap(a))
+// 	// a = append(a, 2, 3, 4, 5)
+// 	// fmt.Println(a)
+// 	// fmt.Println("Length: %v\n", len(a))
+// 	// fmt.Println("Capacity: %v\n", cap(a))
+// 	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+// 	// b := a[:]
+// 	// c := a[3:]
+// 	// d := a[:6]
+// 	// e := a[3:6]
+// 	// a[5] = 42
+// 	// fmt.Println(a)
+// 	// fmt.Println(b)
+// 	// fmt.Println(c)
+// 	// fmt.Println(d)
+// 	// fmt.Println(e)
 
-	// a := []int{1, 2, 3}
-	// b := a
-	// b[1] = 5
-	// fmt.Println(a)
-	// fmt.Println(b)
-	// fmt.Printf("Length: %v\n", len(a))
-	// fmt.Printf("Capacity: %v\n", cap(a))
+// 	// a := []int{1, 2, 3}
+// 	// b := a
+// 	// b[1] = 5
+// 	// fmt.Println(a)
+// 	// fmt.Println(b)
+// 	// fmt.Printf("Length: %v\n", len(a))
+// 	// fmt.Printf("Capacity: %v\n", cap(a))
 
-	// var identityMatrix [3][3]int = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}}
-	// fmt.Println(identityMatrix)
+// 	// var identityMatrix [3][3]int = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}}
+// 	// fmt.Println(identityMatrix)
 
-	// grades := [...]int{97, 85, 93}
-	// var students [5]string
-	// fmt.Printf("Students: %v\n", students)
-	// students[0] = "Lisa"
-	// students[1] = "Ahmad"
-	// students[2] = "Arnold"
-	// fmt.Printf("Students: %v\n", students[1])
-	// fmt.Printf("number of Students: %v\n", len(students))
-}
+// 	// grades := [...]int{97, 85, 93}
+// 	// var students [5]string
+// 	// fmt.Printf("Students: %v\n", students)
+// 	// students[0] = "Lisa"
+// 	// students[1] = "Ahmad"
+// 	// students[2] = "Arnold"
+// 	// fmt.Printf("Students: %v\n", students[1])
+// 	// fmt.Printf("number of Students: %v\n", len(students))
+// }
