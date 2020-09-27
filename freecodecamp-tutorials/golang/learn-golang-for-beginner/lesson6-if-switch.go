b5e813e685d92d5220fd8c9ca44d9cede5864e96
alailsonko <aalailson3@gmail.com>
2020-09-26 23:31:44 -0300

    lesson6 update

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson6-if-switch.go
@@ -0,0 +1,60 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	var i interface{} = "string"
+// 	switch i.(type) {
+// 	case int:
+// 		fmt.Println("i is an int")
+// 		fmt.Println("this will print too")
+// 	case float64:
+// 		fmt.Println("i is an float64")
+// 	case string:
+// 		fmt.Println("i is a string")
+// 	default:
+// 		fmt.Println("i is another type")
+// 	}
+
+// 	// number := 50
+// 	// guess := 3
+
+// 	// if guess < 1 {
+// 	// 	fmt.Println("The guess must be beween 1 and 100!")
+
+// 	// } else if guess > 100 {
+// 	// 	fmt.Println("The guess must be beween 1 and 100!")
+// 	// } else {
+// 	// 	if guess < number {
+// 	// 		fmt.Println("Too low\n")
+// 	// 	}
+// 	// 	if guess > number {
+// 	// 		fmt.Println("Too high\n")
+// 	// 	}
+// 	// 	if guess == number {
+// 	// 		fmt.Println("You got it!\n")
+// 	// 	}
+// 	// 	fmt.Println(number <= guess, number >= guess, number != guess)
+
+// 	// }
+
+// 	// statePopulations := map[string]int{
+// 	// 	"California":   48394389,
+// 	// 	"Texas":        32144389,
+// 	// 	"Florida":      32194389,
+// 	// 	"New York":     65654389,
+// 	// 	"Pennsylvania": 85674389,
+// 	// 	"Illinois":     12324389,
+// 	// 	"Ohio":         53234389,
+// 	// }
+// 	// if pop, ok := statePopulations["Florida"]; ok {
+// 	// 	fmt.Println(pop)
+// 	// }
+// }
+
+// func returnTrue() bool {
+// 	fmt.Println("returning true")
+
+// 	return true
+// }
