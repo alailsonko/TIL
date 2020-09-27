8d7f571b6bdd67453ac71290934a6ccf09f0fc4d
alailsonko <aalailson3@gmail.com>
2020-09-26 23:31:53 -0300

    lesson7 update

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson7-looping.go
@@ -0,0 +1,34 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	s := []int{1, 2, 3}
+// 	for k, v := range s {
+
+// 		fmt.Println(k, v)
+// 	}
+
+// 	// for i := 0; i < 10000000000; i++ {
+// 	// 	if i%2 == 0 {
+// 	// 		continue
+// 	// 	}
+// 	// 	fmt.Println(i)
+// 	// }
+
+// 	// for := 0; i < 5; i++ {
+// 	// 	fmt.Println(i)
+// 	// 	if i%2 == 0 {
+// 	// 		i /= 2
+// 	// 	} else {
+// 	// 		i = 2*i + 1
+// 	// 	}
+// 	// }
+
+// 	// for i, j := 0, 0; i <= 1000000000; i, j = i+1, j {
+// 	// 	fmt.Println(i)
+// 	// 	fmt.Println("swtich\n")
+// 	// 	fmt.Println(j)
+// 	// }
+// }
