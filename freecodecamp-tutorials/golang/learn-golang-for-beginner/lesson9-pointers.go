3f99e925e3291c5c4fd5455767f9e66deff03512
alailsonko <aalailson3@gmail.com>
2020-09-26 23:32:14 -0300

    lesson9 update

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson9-pointers.go
@@ -0,0 +1,23 @@
+// package main
+
+// func main() {
+// var ms *myStruct
+// ms = &myStruct{foo: 42}
+// fmt.Println(ms)
+// a := [3]int{1, 2, 3}
+// b := &a[0]
+// c := &a[1] - 4
+// fmt.Printf("%v %p %p\n", a, b, c)
+// var a int = 42
+// var b *int = &a
+
+// fmt.Println(&a, *b)
+// a = 27
+// fmt.Println(a, *b)
+// *b = 14
+// fmt.Println(a, *b)
+// }
+
+// type myStruct struct {
+// 	foo int
+// }
