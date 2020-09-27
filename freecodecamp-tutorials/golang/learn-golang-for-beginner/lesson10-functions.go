dfcaffa7b93552807031a1d6d1f9bd7a0aca83c4
alailsonko <aalailson3@gmail.com>
2020-09-26 23:32:22 -0300

    lesson10 update

---
+++ freecodecamp-tutorials/golang/learn-golang-for-beginner/lesson10-functions.go
@@ -0,0 +1,20 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	for i := 0; i < 5; i++ {
+// 		sayMessage("hello go", i)
+
+// 	}
+// 	sayGreeting("hello", "alailsonko")
+// }
+
+// func sayGreeting(greeting string, name string) {
+// 	fmt.Println(greeting, name)
+// }
+
+// func sayMessage(msg string, idx int) {
+// 	fmt.Println(msg)
+// 	fmt.Println("The value of the index is", idx)
+// }
