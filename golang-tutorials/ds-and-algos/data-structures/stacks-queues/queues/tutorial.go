f911c3c4194b80ee57caf0298ca308b4dbc9b42d
alailsonko <aalailson3@gmail.com>
2020-09-28 00:40:22 -0300

    how to make queue

---
+++ golang-tutorials/ds-and-algos/data-structures/stacks-queues/queues/tutorial.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Queue represents stacck that hold a slice
+type Queue struct {
+	items []int
+}
+
+// Enqueue adds a value at the end
+func (q *Queue) Enqueue(i int) {
+	q.items = append(q.items, i)
+}
+
+// Dequeue removes a value at the front
+// and RETURNs the removed value
+func (q *Queue) Dequeue() int {
+	toRemove := q.items[0]
+	q.items = q.items[1:]
+	return toRemove
+}
+
+func main() {
+	myQueue := Queue{}
+	fmt.Println(myQueue)
+	myQueue.Enqueue(100)
+	myQueue.Enqueue(200)
+	myQueue.Enqueue(300)
+	fmt.Println(myQueue)
+	myQueue.Dequeue()
+	fmt.Println(myQueue)
+}
