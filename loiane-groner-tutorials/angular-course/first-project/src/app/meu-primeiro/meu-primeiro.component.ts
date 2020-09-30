43cede5d8425a7daae7d010eb13a976972bf5028
alailsonko <aalailson3@gmail.com>
2020-09-29 22:24:33 -0300

    angular: make my first component

---
+++ loiane-groner-tutorials/angular-course/first-project/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+      <p>Meu primeiro component com angular 4</p>
+    `
+})
+
+export class MeuPrimeiroComponent {
+
+}
