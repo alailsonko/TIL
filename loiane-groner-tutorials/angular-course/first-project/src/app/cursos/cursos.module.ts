5c305cee3b8e858c964d711025ddb83ba48217de
alailsonko <aalailson3@gmail.com>
2020-09-30 00:07:38 -0300

    angular: create my first module

---
+++ loiane-groner-tutorials/angular-course/first-project/src/app/cursos/cursos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CursosComponent, CursosDetalheComponent],
+  exports: [CursosComponent]
+})
+export class CursosModule { }
