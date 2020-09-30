1398461c58393b950d5b33b49d860daa9fdca02d
alailsonko <aalailson3@gmail.com>
2020-09-29 23:41:03 -0300

    typescript: fundamentals of typescript

---
+++ loiane-groner-tutorials/angular-course/introduction-to-typescript/main.js
@@ -0,0 +1,19 @@
+var myVar = 'my variable';
+function myFunc(x, y) {
+    return x + y;
+}
+//ES6 and ES2015
+var num = 2;
+var PI = 3.14;
+var numbers = [1, 2, 3];
+numbers.map(function (item) { return item * 2; }); //ES 2015
+var Mathematics = /** @class */ (function () {
+    function Mathematics() {
+    }
+    Mathematics.prototype.sum = function (x, y) {
+        return x + y;
+    };
+    return Mathematics;
+}());
+var n1 = 'jdajdia';
+n1 = 4;
---
+++ loiane-groner-tutorials/angular-course/introduction-to-typescript/main.ts
@@ -0,0 +1,23 @@
+var myVar = 'my variable'
+
+function myFunc(x, y) {
+    return x + y
+}
+
+//ES6 and ES2015
+let num = 2
+const PI = 3.14
+
+var numbers = [1,2,3]
+
+numbers.map(item => item * 2) //ES 2015
+
+
+class Mathematics {
+    sum(x, y) {
+        return x + y
+    }
+}
+
+var n1: any = 'jdajdia'
+n1 = 4
