be1291e446c817bd4d568e2baaa1e098286f105c
alailsonko <aalailson3@gmail.com>
2020-10-01 00:46:21 -0300

    study of event binding

--- loiane-groner-tutorials/angular-course/data-binding/src/app/data-binding/data-binding.component.ts
+++ loiane-groner-tutorials/angular-course/data-binding/src/app/data-binding/data-binding.component.ts
@@ -13,12 +13,33 @@ export class DataBindingComponent implements OnInit {
 
   urlImage: string = 'http://lorempixel.com/g/400/200/nature/'
 
+  actualValue: string = ''
+
+  savedValue: any
+
+  isMouseOver: boolean = false
+
+  saveValue(value) {
+    this.savedValue = value
+  }
+
   getValue() {
     return 1
   }
 
   getLikeCourses() {
     return true
+  }
+
+  clickedButton() {
+    alert('clicked button')
+  }
+  onKeyUp(event: KeyboardEvent) {
+    this.actualValue = (<HTMLInputElement>event.target).value
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
   }
 
   constructor() { }
