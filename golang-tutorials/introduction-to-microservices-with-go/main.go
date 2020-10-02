c44dff1f5d9d441f50a82f2a9e1f208e14d43513
alailsonko <aalailson3@gmail.com>
2020-10-02 13:33:16 -0300

    golang: add get method to products services

--- golang-tutorials/introduction-to-microservices-with-go/main.go
+++ golang-tutorials/introduction-to-microservices-with-go/main.go
@@ -12,13 +12,15 @@ import (
 
 func main() {
 
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	// env.Parse()
 
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new server mux and register the handlers
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
@@ -29,13 +31,16 @@ func main() {
 	}
 
 	go func() {
+		l.Println("Starting server on port 9090")
+
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			l.Printf("Error starting server: %v\n", err)
+			os.Exit(1)
 		}
 	}()
 
-	sigChan := make(chan os.Signal)
+	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, os.Kill)
 
--- golang-tutorials/introduction-to-microservices-with-go/product-api/data.go
+++ golang-tutorials/introduction-to-microservices-with-go/product-api/data.go
@@ -1,16 +1,32 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
+// Product defines the structure for an API product
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       float32
-	SKU         string
-	CreatedOn   string
-	UpdatedOn   string
-	DeleteOn    string
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeleteOn    string  `json:"-"`
+}
+
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
+	return productList
 }
 
 var productList = []*Product{
