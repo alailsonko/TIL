f39d8de16c0a66115d98d01bc0c40394e6d6a463
alailsonko <aalailson3@gmail.com>
2020-10-06 00:25:01 -0300

    golang: implements multipart form request

--- golang-tutorials/introduction-to-microservices-with-go/product-images/go.mod
+++ golang-tutorials/introduction-to-microservices-with-go/product-images/go.mod
@@ -3,6 +3,7 @@ module product-images
 go 1.15
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/go-hclog v0.14.1
 	github.com/nicholasjackson/env v0.6.0
--- golang-tutorials/introduction-to-microservices-with-go/product-images/main.go
+++ golang-tutorials/introduction-to-microservices-with-go/product-images/main.go
@@ -9,12 +9,13 @@ import (
 	"product-images/handlers"
 	"time"
 
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	hclog "github.com/hashicorp/go-hclog"
 	"github.com/nicholasjackson/env"
 )
 
-var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+var bindAddress = env.String("BIND_ADDRESS", false, ":9091", "Bind address for the server")
 var logLevel = env.String("LOG_LEVEL", false, "debug", "Log output level for the server [debug, info, trace]")
 var basePath = env.String("BASE_PATH", false, "./imagestore", "Base path to save images")
 
@@ -46,10 +47,13 @@ func main() {
 	// create a new serve mux and register the handlers
 	sm := mux.NewRouter()
 
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
+
 	// filename regex: {filename:[a-zA-Z]+\\.[a-z]{3}}
 	// problem with FileServer is that it is dumb
 	ph := sm.Methods(http.MethodPost).Subrouter()
-	ph.HandleFunc("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", fh.ServeHTTP)
+	ph.HandleFunc("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", fh.UploadREST)
+	ph.HandleFunc("/", fh.UploadMultipart)
 
 	// get files
 	gh := sm.Methods(http.MethodGet).Subrouter()
@@ -60,8 +64,8 @@ func main() {
 
 	// create a new server
 	s := http.Server{
-		Addr:         ":9090",           // configure the bind address
-		Handler:      sm,                // set the default handler
+		Addr:         ":9091",           // configure the bind address
+		Handler:      ch(sm),            // set the default handler
 		ErrorLog:     sl,                // the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
