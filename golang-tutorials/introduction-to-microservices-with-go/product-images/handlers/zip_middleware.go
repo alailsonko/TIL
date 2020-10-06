fc6384bd5f92263e12905654517e976a5da0ee08
alailsonko <aalailson3@gmail.com>
2020-10-06 02:42:11 -0300

    golang: implement gzip compression

---
+++ golang-tutorials/introduction-to-microservices-with-go/product-images/handlers/zip_middleware.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"compress/gzip"
+	"net/http"
+	"strings"
+)
+
+type GzipHandler struct {
+}
+
+func (g *GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			// create a gzip response
+			wrw := NewWrappedResponseWriter(rw)
+
+			wrw.Header().Set("Content-Encoding", "gzip")
+
+			next.ServeHTTP(wrw, r)
+			defer wrw.Flush()
+			return
+		}
+		// handler normal
+		next.ServeHTTP(rw, r)
+	})
+}
+
+type WrappedResponseWriter struct {
+	rw http.ResponseWriter
+	gw *gzip.Writer
+}
+
+func NewWrappedResponseWriter(rw http.ResponseWriter) *WrappedResponseWriter {
+	gw := gzip.NewWriter(rw)
+
+	return &WrappedResponseWriter{rw: rw, gw: gw}
+}
+
+func (wr *WrappedResponseWriter) Header() http.Header {
+	return wr.rw.Header()
+}
+
+func (wr *WrappedResponseWriter) Write(d []byte) (int, error) {
+	return wr.gw.Write(d)
+}
+
+func (wr *WrappedResponseWriter) WriteHeader(statuscode int) {
+	wr.rw.WriteHeader(statuscode)
+}
+
+func (wr *WrappedResponseWriter) Flush() {
+	wr.gw.Flush()
+	wr.gw.Close()
+}
