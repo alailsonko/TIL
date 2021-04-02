45fed263ff5dbb8023ce88fad1414edb6c8c6ef0
alailsonko <aalailson3@gmail.com>
2021-04-01 21:30:53 -0300

    delete

--- advanced-studies/2_c_c++/books/ds_algo_c/chapter3/3.2/ite_sumsqrt.c
+++ advanced-studies/2_c_c++/books/ds_algo_c/chapter3/3.2/ite_sumsqrt.c
@@ -1,6 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include <string.h>
+#include<string.h>
 
 
 int SumSquares(int m, int n) {
---
+++ hackerrank/nodejs/interview-preparation-kit/warm-up-challenges/test.js
@@ -0,0 +1,9 @@
+function controller() {
+  (function (e) {
+    console.log(e);
+  })("AWARD_BADGE");
+
+  console.log("dispatch 2");
+}
+
+controller();
