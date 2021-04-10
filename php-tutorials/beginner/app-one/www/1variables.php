f73e17dd9e8af29d2ebdfefb263e0f85f39b049b
alailsonko <aalailson3@gmail.com>
2021-04-10 20:41:35 -0300

    php variables

---
+++ php-tutorials/beginner/app-one/www/1variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $characterName = 'John';
+    $characterAge = '35';
+
+    echo "There once was a man named $characterName <br>";
+    echo "He was $characterAge years old <br>";
+    
+    $characterName = 'Mike';
+    $characterAge = '80';
+    
+    echo "He really liked the name $characterName <br>";
+    echo "But didn\'t like being $characterAge <br>";
+    ?>
+</body>
+
+</html>
