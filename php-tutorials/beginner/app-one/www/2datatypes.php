08608dc541d0a4822ea35aef78da941a202ec2e4
alailsonko <aalailson3@gmail.com>
2021-04-10 20:41:43 -0300

    php datatypes

---
+++ php-tutorials/beginner/app-one/www/2datatypes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $phrase = "To be or not to be";
+       $age = 30;
+       $gpa = 32.9;
+       $isMale = true;
+       echo "$phrase <br>";
+       echo "$age <br>";
+       echo "$gpa <br>";
+       echo "$isMale <br>";
+    ?>
+</body>
+</html>
