8fb8bd8642e24261a7a499d878561d3a623eb180
alailsonko <aalailson3@gmail.com>
2021-04-10 20:53:30 -0300

    php strings

--- php-tutorials/beginner/app-one/www/3strings.php
+++ php-tutorials/beginner/app-one/www/3strings.php
@@ -8,7 +8,21 @@
 </head>
 <body>
     <?php
-      echo "Giraffe Academy";
-    ?>
+$phrase = "Giraffe Academy";
+echo strtoupper($phrase);
+echo "<br>";
+echo strlen($phrase);
+echo "<br>";
+echo $phrase[0];
+echo "<br>";
+echo "Alailson"[0];
+echo "<br>";
+$phrase[0] = "B";
+echo $phrase;
+echo "<br>";
+echo str_replace("Biraffe", "Panda", $phrase);
+echo "<br>";
+echo substr($phrase, 5, 4);
+?>
 </body>
 </html>
