684122f337d616fa099b8d33bde643137e8c4227
alailsonko <aalailson3@gmail.com>
2021-04-13 09:43:38 -0300

    php numbers

---
+++ php-tutorials/beginner/app-one/www/4numbers.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo 40;
+        echo "<br>";
+        echo -40.43;
+        echo "<br>";
+        echo 5.7 * 9;
+        echo "<br>";
+        echo 10 % 4;
+        echo "<br>";
+        echo 4 + 5 * 7;
+        echo "<br>";
+        $num = 10;
+        echo $num;
+        echo "<br>";
+        $num++;
+        echo $num;
+        $num--;
+        $num--;
+        $num--;
+        $num--;
+        $num--;
+        echo "<br>";
+        echo $num;
+        echo "<br>";
+        echo $num = $num + 10;
+        echo "<br>";
+        echo abs(-100);
+        echo "<br>";
+        echo pow(2, 4);
+        echo "<br>";
+        echo sqrt(144);
+        echo "<br>";
+        echo max(2, 10, 888);
+        echo "<br>";
+        echo min(2, 10, 888);
+        echo "<br>";
+        echo round(3.7);
+        echo "<br>";
+        echo ceil(3.7);
+        echo "<br>";
+        echo floor(3.7);
+?>
+</body>
+</html>
