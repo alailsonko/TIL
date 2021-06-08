b7c94e296567e39c420b8b3f217aac27f71a55a8
alailsonko <aalailson3@gmail.com>
2021-06-08 16:48:55 -0300

    button component and header component

--- learn-vuejs/hello-world/src/App.vue
+++ learn-vuejs/hello-world/src/App.vue
@@ -1,25 +1,28 @@
 <template>
-   <h1>Hello world</h1>
+  <div class="container">
+     <Header title="Task Tracker" />
+  </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+      Header
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
