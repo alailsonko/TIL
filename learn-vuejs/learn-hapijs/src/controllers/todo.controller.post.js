295a3f716fdc03f5d42c4469902141307197ccb8
alailsonko <aalailson3@gmail.com>
2021-06-15 19:54:40 -0300

    change T to Type

--- learn-vuejs/learn-hapijs/src/controllers/todo.controller.post.js
+++ learn-vuejs/learn-hapijs/src/controllers/todo.controller.post.js
@@ -1,18 +1,34 @@
 // @flow
 
-type IReply = {
-  response(data: any): any;
-  code(statusCode: number): any;
-}
+type TypeReply = {
+  response(data: any): any,
+  code(statusCode: number): any,
+};
 
-type IRequest = {
-  payload: any;
-}
+type TypeRequest = {
+  payload: any,
+};
+
+type TypeTODO = {
+  task: String,
+  date: String,
+  reminder: Boolean,
+};
 
 class CreateTodoController {
   constructor() {}
-  handle(request: IRequest, reply: IReply): any {
-    return reply.response(request.payload).code(400);
+  handle(request: TypeRequest, reply: TypeReply): any {
+    const data: TypeTODO = request.payload;
+    if (!data.task) {
+      return reply.response({ error: "task value is missing." }).code(400);
+    }
+    if (!data.date) {
+      return reply.response({ error: "date value is missing." }).code(400);
+    }
+    if (data.reminder === null) {
+      return reply.response({ error: "reminder value is missing." }).code(400);
+    }
+    return reply.response(request.payload).code(200);
   }
 }
 
