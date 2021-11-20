09a014985372490cd7df1faa8102cd5220e9aa65
alailson <aalailson3@gmail.com>
2021-11-20 00:00:22 -0300

    start project

---
+++ qt/build-getting-started-Desktop-Release/CMakeFiles/getting-started.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getting-started_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/getting-started_autogen.dir/ParseCache.txt"
+  "getting-started_autogen"
+  "CMakeFiles/getting-started.dir/getting-started_autogen/EWIEGA46WW/qrc_qml.cpp.o"
+  "CMakeFiles/getting-started.dir/getting-started_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/getting-started.dir/main.cpp.o"
+  "getting-started"
+  "getting-started.pdb"
+  "getting-started_autogen/EWIEGA46WW/qrc_qml.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getting-started.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qt/build-getting-started-Desktop-Release/CMakeFiles/getting-started_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getting-started_autogen"
+  "getting-started_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/getting-started_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
