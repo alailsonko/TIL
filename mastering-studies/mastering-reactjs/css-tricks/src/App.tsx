56da420287f9543cfb524cb53a6e914ce343b434
alailson <aalailson3@gmail.com>
2021-12-02 23:30:02 -0300

    flex box studies

--- mastering-studies/mastering-reactjs/css-tricks/src/App.tsx
+++ mastering-studies/mastering-reactjs/css-tricks/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="square">
+        <div className="item">1</div>
+        <div className="item">2</div>
+        <div className="item">3</div>
+        <div className="item">4</div>
+        <div className="item">5</div>
+        <div className="item">6</div>
+        <div className="item">7</div>
+        <div className="item">8</div>
+      </div>
     </div>
   );
 }
