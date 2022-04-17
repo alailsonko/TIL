f2dbc39d10093bb1a15f83c1e889fdb6468c790c
alailson <aalailson3@gmail.com>
2022-04-16 22:38:11 -0300

    chapter 1 and 2 examples

---
+++ advanced-studies/2_c_c++/books/learn_c/chapter1/examples/p1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, s;
+    printf("Enter length of side: ");
+    scanf("%d", &s);
+    a = s * s;
+    printf("\nArea of square is %d\n", a);
+    return 0;
+}
---
+++ advanced-studies/2_c_c++/books/learn_c/chapter2/examples/c_tokens.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   int a, b, sum; 
+   a = 14;
+   b = 25;
+   sum = a + b;
+   printf("%5d + %5d = %5d\n", a, b, sum);
+}
